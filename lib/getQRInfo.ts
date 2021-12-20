f9c91e0b058c91c1ff384e3e3d0147d04d538488
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2021-12-20 23:41:45 +0100

    core: qr code data endpoint

---
+++ lib/getQRInfo.ts
@@ -0,0 +1,30 @@
+interface Name {
+  fn: string; // last name
+  gn: string; // first name
+}
+
+export interface QRData {
+  dob: string;
+  nam: Name;
+}
+
+export const getDataFromQR = async (qrData: string): Promise<QRData> => {
+  try {
+    const res = await fetch(process.env.API_URL, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        qrData,
+      }),
+    });
+
+    const data = await res.json();
+
+    return data.res;
+  } catch (error) {
+    throw error;
+  }
+};
