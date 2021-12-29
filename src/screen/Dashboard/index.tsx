7e4e6099bceebdcc6a3048ec37a089ada160b25e
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2021-12-29 18:33:30 +0100

    refactor: dashboard view

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -1,13 +1,35 @@
-import { Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { Button, Text, View } from 'react-native';
 
+import BottonSheet from './components/BottomSheet';
+import useDashboard from './useDashboard';
 import { useQrStore } from '../../store/qr';
 
 const Dashboard = () => {
-  const qrData = useQrStore((state) => state.qrData);
+  const qrList = useQrStore((state) => state.qrList);
+  const { handleCreateNewList, bottomSheetRef, handleCreateNewListConfirm } =
+    useDashboard();
+
+  const renderQrList = useCallback(() => {
+    return qrList.map((qr) => {
+      return (
+        <View style={{ backgroundColor: 'yellow' }} key={qr.id}>
+          <Text>{qr.name}</Text>
+        </View>
+      );
+    });
+  }, [qrList]);
 
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, backgroundColor: 'red' }}>
       <Text>List</Text>
+      <Button onPress={handleCreateNewList} title="Crear nueva lista" />
+      {renderQrList()}
+
+      <BottonSheet
+        bottomSheetRef={bottomSheetRef}
+        handleConfirm={handleCreateNewListConfirm}
+      />
     </View>
   );
 };
