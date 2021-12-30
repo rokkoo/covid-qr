0960cc0b9d9cab56de103f16f8a21cd7a9bcca5b
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2021-12-30 20:21:48 +0100

    fix: scroll view on dashboard

--- src/screen/Dashboard/index.tsx
+++ src/screen/Dashboard/index.tsx
@@ -4,13 +4,16 @@ import BottonSheet from './components/BottomSheet';
 import useDashboard from './useDashboard';
 import List from './components/List';
 import OwnCertificateSection from './components/OwnCertificateSection';
+import { ScrollView } from 'react-native';
 
 const Dashboard = () => {
   return (
-    <Box safeArea h="full" bg="dark.900" px={4} pt={4}>
-      <OwnCertificateSection />
-      <List />
-    </Box>
+    <ScrollView>
+      <Box safeArea h="full" bg="dark.900" px={4} pt={4}>
+        <OwnCertificateSection />
+        <List />
+      </Box>
+    </ScrollView>
   );
 };
 
